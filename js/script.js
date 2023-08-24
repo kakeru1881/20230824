763070e75bf507a6d29ad9557fe35ba21e556c76
kakeru1881 <hamiribu.kakeru@gmail.com>
2023-08-24 21:56:04 +0900

    deploy

---
+++ js/script.js
@@ -0,0 +1,55 @@
+$(function () {
+
+  //ページ内スクロール
+  var navHeight = $(".header").outerHeight();
+
+  $('a[href^="#"]').on("click", function () {
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? "html" : href);
+    var position = target.offset().top - navHeight;
+    $("html, body").animate({ scrollTop: position, }, 300, "swing");
+    return false;
+  });
+
+  //ページトップ
+  $("#js-page-top").on("click", function () {
+    $("body,html").animate({ scrollTop: 0, }, 300);
+    return false;
+  });
+});
+
+// $('.works-item').magnificPopup({
+//   type: 'image',
+//   gallery: {
+//     enabled: true
+
+//   }
+// });
+
+// $(function () {
+//   $('.works-item').magnificPopup({
+//     delegate: 'a',
+//     type: 'image',
+//     gallery: {
+//       enabled: true,
+//       arrowMarkup: '<span title="%title%" role="button" class="gallery-arrow gallery-arrow-%dir%"></span>'
+//     }
+//   });
+// });
+
+// ポップアップを初期化する
+$(document).ready(function () {
+  $('.works-item').magnificPopup({
+    type: 'image', // 画像を表示
+    gallery: {
+      enabled: true // ギャラリーモード有効化（画像間の切り替えができる）
+    }
+  });
+});
+
+
+// $(function () {
+//   $('.works-item').magnificPopup({
+//     type: 'image',
+//   });
+// });
